d33e53a1bf9e9203cc82070310b0fdad27cf181b
Hang <whitewield@126.com>
2017-04-11 16:49:06 -0400

    Clam

---
+++ Assets/Scripts/PP_CannonZone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PP_CannonZone : MonoBehaviour {
+
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	void OnTriggerStay2D (Collider2D g_Collider2D) {
+		Debug.Log ("PP_CannonZone");
+		if (g_Collider2D.tag == PP_Global.TAG_PLAYER) {
+			PP_Cannon.Instance.ShellCharge ();
+		}
+	}
+}
