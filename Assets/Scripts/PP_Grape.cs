9c8109efacf23f93b56216e5622182113d9a16c0
Hang <whitewield@126.com>
2017-05-15 17:08:35 -0400

    GrapeUpdate

--- Assets/Scripts/PP_Grape.cs
+++ Assets/Scripts/PP_Grape.cs
@@ -7,6 +7,40 @@ public class PP_Grape : MonoBehaviour {
 	[SerializeField] GameObject myEffect;
 	private PP_GrapeManager myManager;
 
+	[SerializeField] GameObject mySpriteGO;
+	[SerializeField] GameObject myDirectionGO;
+	private Rigidbody2D myRigidbody2D;
+	private Vector2 myMoveDireciton;
+	private float myMoveSpeed = 0;
+	[SerializeField] float myScaleRatio = 0.1f;
+	[SerializeField] float myScaleSpeed = 1f;
+	float moveSpeedX;
+	float moveSpeedY;
+
+	void Awake () {
+		myRigidbody2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		myMoveDireciton = myRigidbody2D.velocity.normalized;
+		myMoveSpeed = myRigidbody2D.velocity.sqrMagnitude;
+
+		Quaternion t_quaternion = Quaternion.Euler (0, 0, 
+			Vector2.Angle (Vector2.up, myMoveDireciton) * Mathf.Sign (myMoveDireciton.x * -1));
+
+//		myDirectionGO.transform.rotation = t_quaternion;
+		myDirectionGO.transform.rotation = 
+			Quaternion.Lerp (myDirectionGO.transform.rotation, t_quaternion, Time.fixedDeltaTime * myScaleSpeed);
+		
+		float t_ratio = myMoveSpeed * myScaleRatio + 1;
+
+		Vector3 t_scale = new Vector3 (1 / t_ratio, t_ratio, 1);
+
+		myDirectionGO.transform.localScale = 
+			Vector3.Lerp (myDirectionGO.transform.localScale, t_scale, Time.fixedDeltaTime * myScaleSpeed);
+
+		mySpriteGO.transform.rotation = this.transform.rotation;
+	}
 
 	public void SetMyManager (PP_GrapeManager g_manager) {
 		myManager = g_manager;
--- Assets/Scripts/PP_UrchinSprite.cs
+++ Assets/Scripts/PP_UrchinSprite.cs
@@ -4,46 +4,57 @@ using UnityEngine;
 
 public class PP_UrchinSprite : MonoBehaviour {
 
+
 //	SpriteRenderer spriteRend;
-	GameObject spriteGO;
-	GameObject fixedGO;
-	Rigidbody2D rb;
-	Vector2 moveDir;
+	[SerializeField] GameObject mySpriteGO;
+	[SerializeField] GameObject myDirectionGO;
+	private Rigidbody2D myRigidbody2D;
+	private Vector2 myMoveDireciton;
+	private float myMoveSpeed = 0;
+	[SerializeField] float myScaleRatio = 0.1f;
+	[SerializeField] float myScaleSpeed = 1f;
 
 	float moveSpeedX;
 	float moveSpeedY;
-	float xMod;
-
-	float yMod;
 
 
-	void Start () {
-		rb = GetComponent<Rigidbody2D> ();
+	void Awake () {
+		myRigidbody2D = GetComponent<Rigidbody2D> ();
 //		spriteRend = GetComponentInChildren<SpriteRenderer> ();
-
-		spriteGO = transform.GetChild (0).GetChild(0).gameObject;
-		fixedGO = transform.GetChild (0).gameObject;
 	}
 	
 
-	void Update () {
-		moveDir = rb.velocity;
+	void FixedUpdate () {
+		myMoveDireciton = myRigidbody2D.velocity.normalized;
+		myMoveSpeed = myRigidbody2D.velocity.sqrMagnitude;
 
-		moveSpeedX = Mathf.Abs (moveDir.x);
-		moveSpeedY = Mathf.Abs (moveDir.y);
+		Quaternion t_quaternion = Quaternion.Euler (0, 0, 
+			Vector2.Angle (Vector2.up, myMoveDireciton) * Mathf.Sign (myMoveDireciton.x * -1));
 
-		int direction = 1;
-		if (moveSpeedX > moveSpeedY)
-			direction = 1;
-		else
-			direction = 1;
+//		myDirectionGO.transform.rotation = t_quaternion;
+		myDirectionGO.transform.rotation = 
+			Quaternion.Lerp (this.transform.rotation, t_quaternion, Time.fixedDeltaTime * myScaleSpeed);
+		float t_ratio = myMoveSpeed * myScaleRatio + 1;
+		myDirectionGO.transform.localScale = new Vector3 (1 / t_ratio, t_ratio, 1);
 
-		Vector2 newScale = new Vector2 (1+moveSpeedX*0.03f - moveSpeedY*0.03f, 1+moveSpeedY*0.03f - moveSpeedX*0.03f);
+		mySpriteGO.transform.rotation = this.transform.rotation;
+
+//		moveSpeedX = Mathf.Abs (moveDir.x);
+//		moveSpeedY = Mathf.Abs (moveDir.y);
+//
+//		int direction = 1;
+//		if (moveSpeedX > moveSpeedY)
+//			direction = 1;
+//		else
+//			direction = 1;
+//
+//		Vector2 newScale = new Vector2 (1+moveSpeedX*0.03f - moveSpeedY*0.03f, 1+moveSpeedY*0.03f - moveSpeedX*0.03f);
 
 
-		fixedGO.transform.localScale = newScale;
-		fixedGO.transform.rotation = Quaternion.Euler( new Vector2 (0, 0));
-		spriteGO.transform.rotation = gameObject.transform.rotation;
+
+//		fixedGO.transform.localScale = newScale;
+//		fixedGO.transform.rotation = Quaternion.Euler( new Vector2 (0, 0));
+//		spriteGO.transform.rotation = gameObject.transform.rotation;
 
 	}
 
